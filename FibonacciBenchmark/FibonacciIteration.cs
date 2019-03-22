6846d9339d018be76aa28e7e94739e4bcff9abcc
Stephen Lin <stephen.sunglun.lin@gmail.com>
2019-03-22 16:03:32 +0800

    applying interface IFibonacci

---
+++ FibonacciBenchmark/FibonacciIteration.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace FibonacciBenchmark
+{
+    public class FibonacciIteration : IFibonacciGenerator
+    {
+        public ulong Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0UL;
+            else if (n == 1)
+                return 1UL;
+            else
+            {
+                var prev = 1UL;
+                var prevprev = 0UL;
+                var result = 1UL;
+                var i = 1;
+                while (i < n)
+                {
+                    try
+                    {
+                        result = checked(prev + prevprev);
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine($"Calculation overflow occured at n={i}");
+                        throw;
+                    }
+
+                    prevprev = prev;
+                    prev = result;
+                    i++;
+                }
+
+                return result;
+            }
+        }
+    }
+}
